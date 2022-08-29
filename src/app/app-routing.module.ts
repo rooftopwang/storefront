b8c6128f91bf034045a33c39be0c64bbf464c7d1
wolfgang <wolfgangwang@hotmail.ca>
2022-08-29 14:02:33 +0800

    components init

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CartComponent } from './components/cart/cart.component'
+import { ConfirmationComponent } from './components/confirmation/confirmation.component'
+import { ProductItemComponent } from './components/product-item/product-item.component'
+import { ProductItemDetailComponent } from './components/product-item-detail/product-item-detail.component'
+import { ProductListComponent } from './components/product-list/product-list.component'
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  { path: '',  component: ProductListComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
